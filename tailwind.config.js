0808485b401b13ff68e9667e56a9ac2abc829194
Nishant Singh Ujjwal <nishantujjwal2000@gmail.com>
2023-02-16 18:18:39 +0530

    challenge 2

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        Cyan: "hsl(179, 62%, 43%)",
+        BrightYellow: "hsl(71, 73%, 54%)",
+        LightGray: "hsl(204, 43%, 93%)",
+        GrayishBlue: "hsl(218, 22%, 67%)",
+      },
+      fontFamily: {
+        Karla: "'Karla', sans-serif",
+      },
+    },
+  },
+  plugins: [],
+};
